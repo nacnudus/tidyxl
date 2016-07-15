18d59542d8b7a04391d5ec43b06432e3e330b6c1
nacnudus <nacnudus@gmail.com>
2016-07-15 17:58:51 +0100

    Add cell class header and implementation, parse addresses and count cells

--- src/xlsxsheet.cpp
+++ src/xlsxsheet.cpp
@@ -1,6 +1,7 @@
 #include <Rcpp.h>
 #include "zip.h"
 #include "rapidxml.h"
+#include "xlsxcell.h"
 #include "xlsxsheet.h"
 #include "xlsxbook.h"
 
@@ -23,9 +24,39 @@ xlsxsheet::xlsxsheet(
 
   // Look up name among worksheets in book
   name_ = book.sheets()[i];
+
+  cacheCellcount();
+  parseSheetData();
 }
 
 List xlsxsheet::information() {
   NumericVector x = NumericVector::create(1);
-  return List::create(Named(name_) = "Hello");
+  return List::create(Named(name_) = std::to_string(cellcount_));
+}
+
+void xlsxsheet::cacheCellcount() {
+  // Iterate over all rows and cells to count
+  cellcount_ = 0;
+  for (rapidxml::xml_node<>* row = sheetData_->first_node("row");
+      row; row = row->next_sibling("row")) {
+    for (rapidxml::xml_node<>* c = row->first_node("c");
+        c; c = c->next_sibling("c")) {
+      ++cellcount_;
+    }
+  }
+}
+
+void xlsxsheet::parseSheetData() {
+  // Iterate through rows and cells in sheetData_
+  unsigned long long int i = 0; // counter for checkUserInterrupt
+  for (rapidxml::xml_node<>* row = sheetData_->first_node("row");
+      row; row = row->next_sibling("row")) {
+    if ((i + 1) % 1000 == 0)
+      Rcpp::checkUserInterrupt();
+    for (rapidxml::xml_node<>* c = row->first_node("c");
+        c; c = c->next_sibling("c")) {
+      xlsxcell cell(c);
+    }
+    ++i;
+  }
 }
--- src/xlsxsheet.h
+++ src/xlsxsheet.h
@@ -11,12 +11,18 @@ std::string name_;
 rapidxml::xml_document<> xml_;
 rapidxml::xml_node<>* worksheet_;
 rapidxml::xml_node<>* sheetData_;
+unsigned long long int cellcount_;
 
 public:
 
   xlsxsheet(const std::string& bookPath,
       const int& i, xlsxbook& book);
   Rcpp::List information();
+
+private:
+
+  void cacheCellcount();
+  void parseSheetData();
 
 };
 
