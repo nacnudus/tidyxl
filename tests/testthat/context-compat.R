0c1ed218e1149495c7a225098a79cee9cdbc3c24
nacnudus <nacnudus@gmail.com>
2017-01-17 19:58:39 +0000

    Change test from expect_that to test_that

--- tests/testthat/context-compat.R
+++ tests/testthat/context-compat.R
@@ -2,7 +2,7 @@
 
 context("Compatability")
 
-expect_that("can read document from google doc", {
+test_that("can read document from google doc", {
   # All I want to check is that it doesn't crash, but there's no expect_survival
   expect_silent(tidy_xlsx("iris-google-doc.xlsx"))
 })
