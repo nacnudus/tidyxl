aa3ddf0dc6c062942136e3c9da7142bdb87c5fc6
nacnudus <nacnudus@gmail.com>
2017-01-20 23:19:00 +0000

    Use member initialization where possible

--- src/border.cpp
+++ src/border.cpp
@@ -10,31 +10,29 @@ border::border() {} // default constructor
 
 border::border(rapidxml::xml_node<>* border,
     styles* styles
-    ) {
-  // Initialize variables
+    ):
+  left_(border->first_node("left"), styles),
+  right_(border->first_node("right"), styles),
+  start_(border->first_node("start"), styles), // gnumeric
+  end_(border->first_node("end"), styles),     // gnumeric
+  top_(border->first_node("top"), styles),
+  bottom_(border->first_node("bottom"), styles),
+  diagonal_(border->first_node("diagonal"), styles),
+  vertical_(border->first_node("vertical"), styles),
+  horizontal_(border->first_node("horizontal"), styles)
+{
   diagonalDown_ = IntegerVector::create(NA_INTEGER);
-  diagonalUp_   = IntegerVector::create(NA_INTEGER);
-  outline_      = IntegerVector::create(NA_INTEGER);
-
   rapidxml::xml_attribute<>* diagonalDown = border->first_attribute("diagonalDown");
   if (diagonalDown != NULL)
     diagonalDown_[0] = strtol(diagonalDown->value(), NULL, 10);
 
+  diagonalUp_   = IntegerVector::create(NA_INTEGER);
   rapidxml::xml_attribute<>* diagonalUp = border->first_attribute("diagonalUp");
   if (diagonalUp != NULL)
     diagonalUp_[0] = strtol(diagonalUp->value(), NULL, 10);
 
+  outline_      = IntegerVector::create(NA_INTEGER);
   rapidxml::xml_attribute<>* outline = border->first_attribute("outline");
   if (outline != NULL)
     outline_[0] = strtol(outline->value(), NULL, 10);
-
-  left_ = stroke(border->first_node("left"), styles);
-  right_ = stroke(border->first_node("right"), styles);
-  start_ = stroke(border->first_node("start"), styles); // gnumeric
-  end_ = stroke(border->first_node("end"), styles);     // gnumeric
-  top_ = stroke(border->first_node("top"), styles);
-  bottom_ = stroke(border->first_node("bottom"), styles);
-  diagonal_ = stroke(border->first_node("diagonal"), styles);
-  vertical_ = stroke(border->first_node("vertical"), styles);
-  horizontal_ = stroke(border->first_node("horizontal"), styles);
 }
