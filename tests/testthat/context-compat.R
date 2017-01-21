e8a3961bdaa02abd31aaf59bd27222b6b5fb4911
nacnudus <nacnudus@gmail.com>
2017-01-21 22:59:44 +0000

    Move examples.xlsx test to context-compat.R

--- tests/testthat/context-compat.R
+++ tests/testthat/context-compat.R
@@ -2,6 +2,10 @@
 
 context("Compatability")
 
+test_that("can import examples.xlsx", {
+  expect_error(x <- tidy_xlsx("./examples.xlsx"), NA)
+})
+
 test_that("can read document from google doc", {
   # All I want to check is that it doesn't crash, but there's no expect_survival
   expect_silent(tidy_xlsx("iris-google-doc.xlsx"))
--- tests/testthat/test-examples.R
+++
@@ -1,6 +0,0 @@
-context("examples.xlsx")
-
-test_that("can import examples.xlsx", {
-  expect_error(x <- tidy_xlsx("./examples.xlsx"), NA)
-})
-
