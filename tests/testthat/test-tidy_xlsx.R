4a2a1adbcc4dd254189a75be297bffa431935357
nacnudus <nacnudus@gmail.com>
2017-01-22 00:41:54 +0000

    Test missing sheets

---
+++ tests/testthat/test-tidy_xlsx.R
@@ -0,0 +1,8 @@
+context("Zip")
+
+test_that("warns of missing sheets", {
+  expect_warning(expect_error(tidy_xlsx("./examples.xlsx", c(NA, NA)),"All elements of argument 'sheets' were discarded."),  "Argument 'sheets' included NAs, which were discarded.")
+  expect_error(tidy_xlsx("./examples.xlsx", "foo"), "Sheet\\(s\\) not found: \"foo\"")
+  expect_error(tidy_xlsx("./examples.xlsx", c("foo", "bar")), "Sheet\\(s\\) not found: \"foo\", \"bar\"")
+})
+
