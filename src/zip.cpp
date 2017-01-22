6624871ee58549946f6905e3c91d55cc30973561
nacnudus <nacnudus@gmail.com>
2017-01-22 00:07:08 +0000

    Remove unused zip.cpp code

--- src/zip.cpp
+++ src/zip.cpp
@@ -37,25 +37,3 @@ bool zip_has_file(
   LogicalVector res = wrap<LogicalVector>(zip_has_file(zip_path, file_path));
   return res[0];
 }
-
-std::string xml_print(std::string xml) {
-  rapidxml::xml_document<> doc;
-
-  xml.push_back('\0');
-  doc.parse<0>(&xml[0]);
-
-  std::string s;
-  rapidxml::print(std::back_inserter(s), doc, 0);
-
-  return s;
-}
-
-// [[Rcpp::export]]
-void zip_xml(
-    const std::string& zip_path,
-    const std::string& file_path) {
-
-  std::string buffer = zip_buffer(zip_path, file_path);
-  Rcout << xml_print(buffer);
-}
-
