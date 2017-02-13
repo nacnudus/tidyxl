793242986a579279c14ab6103c4f5af26dfee138
nacnudus <nacnudus@gmail.com>
2017-02-13 20:39:19 +0000

    Control date bug warnings in test of date

--- tests/testthat/test-dates.R
+++ tests/testthat/test-dates.R
@@ -14,7 +14,9 @@ context("Dates")
 test_that("1900 and 1904 dates are parsed correctly", {
   expect_equal(tidy_xlsx("./1900.xlsx")$data$Sheet1$date[1], as.POSIXct("1998-07-05", tz = "UTC"))
   expect_equal(tidy_xlsx("./1904.xlsx")$data$Sheet1$date[1], as.POSIXct("1998-07-05", tz = "UTC"))
-  expect_equal(tidy_xlsx("./openxlsx1900.xlsx")$data$Sheet1$date[2], as.POSIXct("1899-12-31", tz = "UTC"))
+  expect_warning(expect_equal(tidy_xlsx("./openxlsx1900.xlsx")$data$Sheet1$date[2],
+                              as.POSIXct("1899-12-31", tz = "UTC")),
+                 "Dates before 1 March 1900 are off by one, due to Excel's famous bug.")
 })
 
 test_that("A warning is given for 1900-system dates before 1 March 1900", {
