979bef521174bd598ff11e763e1b07acecab7dd2
Duncan Garmonsway <nacnudus@gmail.com>
2017-09-06 00:59:56 +0100

    Add reserved future function names to excel_functions

--- R/data.R
+++ R/data.R
@@ -6,8 +6,11 @@
 #' useful for identifying user-defined functions in formulas tokenized by
 #' [xlex()].
 #'
-#' @format A character vector of length 483.
+#' Note that this includes future function names that are already reserved.
+#'
+#' @format A character vector of length 600.
 #' @source Pages 26--27 of Microsoft's document "Excel (.xlsx) extensions to the
 #' office openxml spreadsheetml file format p.24"
-#' \url{https://msdn.microsoft.com/en-us/library/dd922181(v=office.12).aspx}
+#' \url{https://msdn.microsoft.com/en-us/library/dd922181(v=office.12).aspx},
+#' revision 8.0 2017-06-20.
 "excel_functions"
