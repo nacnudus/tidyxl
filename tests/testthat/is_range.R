74bb1586c0c0d381d4086bc7594bbaae77108e67
Duncan Garmonsway <nacnudus@gmail.com>
2017-11-03 18:10:46 +0000

    Make is_range() tests more pretty

--- tests/testthat/is_range.R
+++ tests/testthat/is_range.R
@@ -1,6 +1,10 @@
 context("is_range()")
 
 test_that("is_range() correctly identifies ranges", {
-  x <- c("A1", "Sheet1!A1", "[0]Sheet1!A1", "A1,A2", "A:A 3:3", "MAX(A1,2)")
-  expect_equal(is_range(x), c(TRUE, TRUE, TRUE, TRUE, TRUE, FALSE))
+  expect_equal(is_range("A1"),           TRUE)
+  expect_equal(is_range("Sheet1!A1"),    TRUE)
+  expect_equal(is_range("[0]Sheet1!A1"), TRUE)
+  expect_equal(is_range("A1,A2"),        TRUE)
+  expect_equal(is_range("A:A 3:3"),      TRUE)
+  expect_equal(is_range("MAX(A1,2)"),    FALSE)
 })
