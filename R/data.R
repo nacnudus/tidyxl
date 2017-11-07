8ad7e4aa14ebe03450d1612e1789e76fcf0363fa
Duncan Garmonsway <nacnudus@gmail.com>
2017-11-07 22:18:58 +0000

    Create xlsx_color_standard dataset
    For looking up the names and RGB values of the standard Excel palette

--- R/data.R
+++ R/data.R
@@ -14,3 +14,16 @@
 #' \url{https://msdn.microsoft.com/en-us/library/dd922181(v=office.12).aspx},
 #' revision 8.0 2017-06-20.
 "excel_functions"
+
+#' Names and RGB values of Excel standard colours
+#'
+#' A dataset containing the names and RGB colour values of Excel's standard
+#' palette.
+#'
+#' @format A data frame with 10 rows and 2 variables:
+#'   * `name` Name of the colour
+#'   * `rgb`  RGB value of the colour
+"xlsx_color_standard"
+
+#' @rdname xlsx_color_standard
+"xlsx_colour_standard"
