6c1d94a06d6a0e9fb4dbd883023db736fe614c7f
Duncan Garmonsway <nacnudus@gmail.com>
2017-12-01 08:41:17 +0000

    Update package docs

--- R/tidyxl-package.R
+++ R/tidyxl-package.R
@@ -10,14 +10,19 @@ NULL
 #' recognising information expressed by relative cell positions and cell
 #' formatting, and re-expressing it in a tidy way.
 #'
-#' * [tidyxl::tidy_xlsx()] Import cells and formatting from an xlsx file.
+#' * [tidyxl::xlsx_cells()] Import cells from an xlsx file.
+#' * [tidyxl::xlsx_formats()] Import formatting from an xlsx file.
 #' * [tidyxl::xlsx_sheet_names()] List the names of sheets in an xlsx file.
-#' * [tidyxl::xlsx_names()] Import names and definitions of named ranges (aka named
+#' * [tidyxl::xlsx_names()] Import names and definitions of named ranges (aka
+#'   'named formulas', 'defined names') from an xlsx file.
 #' * [tidyxl::is_range()] Test whether a 'name' from [tidyxl::xlsx_names()]
-#' refers to a range or not.
-#' formulas, defined names) from an xlsx file.
+#'   refers to a range or not.
 #' * [tidyxl::xlsx_validation()] Import cell input validation rules (e.g.
-#' 'must be from this drop-down list') from an xlsx file.
+#'   'must be from this drop-down list') from an xlsx file.
+#' * [tidyxl::xlsx_colour_standard()] A data frame of standard colour names and
+#'   their RGB values.
+#' * [tidyxl::xlsx_colour_theme()] Imports a data frame of theme colour names
+#'   and their RGB values from an xlsx file.
 #' * [tidyxl::xlex()] Tokenise (lex) an Excel formula.
 #'
 #' @docType package
